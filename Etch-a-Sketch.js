6f73f4664ee3852ef80bb54fd0090e37f82fe232
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-04-24 14:54:58 +0900

    make button and new grid

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -5,16 +5,47 @@ const body = document.querySelector("body");
 btn.textContent = "Choose your grid size"
 btn.setAttribute("style", "max-width:300px", "max-height:150px")
 
-btn.addEventListener("click", () => {
-    prompt("Wtite the size of gird you want:","");
-});
-
 for (let i=0; i < 256; i++) {
     let div = document.createElement("div");
     div.setAttribute("class", "grid");
     
     container.appendChild(div);
-}
+};
+
+btn.addEventListener("click", () => {
+    grids.forEach((grid) => {
+        container.removeChild(grid);
+    });       
+    sizeSelection();
+
+
+
+});
+
+function sizeSelection () {
+    let size = prompt("Write the size of grid you want: (within 100)", "");
+    for (let i=0; i < Number(size) * Number(size); i++) {
+        const div1 = document.createElement("div");
+        container.appendChild(div1);
+        div1.setAttribute("class", "newGrid");
+    };
+
+    const newGrid = document.querySelectorAll(".newGrid");
+    let nSize = (592/size) - 2;
+    newGrid.forEach((grid) => {
+        grid.setAttribute("style", "border: 1px solid brown; width:"+ nSize+"px;height:" + nSize +"px;");
+        grid.addEventListener("mouseenter", () => {
+            grid.style["background-color"] = "blue";
+        });
+    
+        grid.addEventListener("click", () => {
+            grid.style["background-color"] = null;
+        });
+    })
+             
+};
+
+
 
 const grids = document.querySelectorAll(".grid");
 grids.forEach((grid) => {
