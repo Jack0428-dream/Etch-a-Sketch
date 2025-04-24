75a7d56ac329fe8ec800b4574712d89a07eee500
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-04-24 11:43:04 +0900

    put button, make grid, and hovering event

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,36 +1,27 @@
 const container = document.querySelector("#container");
 
-const div1 = document.createElement("div");
-const div2 = document.createElement("div");
-const div3 = document.createElement("div");
-const div4 = document.createElement("div");
-const div5 = document.createElement("div");
-const div6 = document.createElement("div");
-const div7 = document.createElement("div");
-const div8 = document.createElement("div");
-const div9 = document.createElement("div");
-const div10 = document.createElement("div");
-const div11 = document.createElement("div");
-const div12 = document.createElement("div");
-const div13 = document.createElement("div");
-const div14= document.createElement("div");
-const div15 = document.createElement("div");
-const div16 = document.createElement("div");
+const btn = document.querySelector("button");
+const body = document.querySelector("body");
+btn.textContent = "Choose your grid size"
+btn.setAttribute("style", "max-width:300px", "max-height:150px")
 
-container.appendChild(div1);
-container.appendChild(div2);
-container.appendChild(div3);
-container.appendChild(div4);
-container.appendChild(div5);
-container.appendChild(div6);
-container.appendChild(div7);
-container.appendChild(div8);
-container.appendChild(div9);
-container.appendChild(div10);
-container.appendChild(div11);
-container.appendChild(div12);
-container.appendChild(div13);
-container.appendChild(div14);
-container.appendChild(div15);
-container.appendChild(div16);
+// btn.addEventListener("click", () => {
+
+// });
+
+for (let i=0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "grid");
+    
+    container.appendChild(div);
+}
+
+const grids = document.querySelectorAll(".grid");
+grids.forEach((grid) => {
+    grid.addEventListener("mouseenter", () => {
+        grid.style["background-color"] = "blue";
+    });
+})
+
+
 
